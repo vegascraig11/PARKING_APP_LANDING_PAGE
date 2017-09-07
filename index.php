665aa70ed44d453497d71ee020ce3c88ccfb7be8
Craig Robinson <vegascraig11@gmail.com>
2017-09-06 22:15:30 -0700

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>MYCAR</title>
+    <title>MYCAR PARKING</title>
     <link rel="stylesheet" href="css/bootstrap.min.css"/>
     <link rel="stylesheet" href="css/style.css"/>
 </head>
@@ -31,7 +31,7 @@
                 <span>SM</span>ARTER</h2>
             <p class="white">
                 Never forget where your parking spot is again.
-                MyCar makes it easier than ever before.
+                MyCar Parking makes it easier than ever before.
             </p>
 
             </h2>
@@ -70,9 +70,9 @@
             </div>
             <h2>Park & Pin your Location</h2>
             <p>
-                With MyCar parking, use your phones GPS to record the exact location of you parking spot, and then use
+                With MyCar Parking, use your phones GPS to record the exact location of you parking spot, and then use
                 the directions feature to find it.
-                Gone are the days of wondering where you parked! With MyCar you will always know!
+                Gone are the days of wondering where you parked! With MyCar Parking you will always know!
             </p>
 
         </div>
@@ -84,12 +84,12 @@
                 <img class=" location-icon" src="images/icon_parking_type.png"/>
 
             </div>
-            <h2 class="white">Choose parking Type</h2>
+            <h2 class="white">Choose Parking Type</h2>
             <p class="white">
                 With MyCar Parking, you can choose the type of parking session you need whether you are parked on the
                 street or in a garage.
                 Most car parking apps just set your location with GPS, but that does not always work in structured
-                parking garages. MyCar prompts you to input the specific parking information such as floor, spot and any
+                parking garages. MyCar Parking prompts you to input the specific parking information such as floor, spot and any
                 other custom notes that will help you find your car
             </p>
         </div>
@@ -109,8 +109,8 @@
             </div>
             <h2 class="white">Never Forget Again</h2>
             <p class="white">
-                With MyCar parking, we do all of the work for you! You will never walk out of that restaurant, amusemant
-                park, or mall and wonder "Where did I park MyCar again? Simply open MyCar and pin your parking location.
+                With MyCar Parking, we do all of the work for you! You will never walk out of that restaurant, amusemant
+                park, or mall and wonder "Where did I park MyCar Parking again? Simply open MyCar and pin your parking location.
                 When it is time to return to your car, it will be displayed on the map with a "Get Directions" button.
                 Never forget where you are parked again!
             </p>
@@ -119,7 +119,7 @@
     <div class="row download-background">
         <div class="text-center">
             <img src="images/icon_logo2.png" class="icon-container"/>
-            <h2 class="white">Download The MYCAR App</h2>
+            <h2 class="white">Download The MYCAR Parking App</h2>
             <p class="helvetica white">Get a link to download the app sent to you.</p>
             <button class="white-bordered btn no-background" data-toggle="modal" data-target="#myModal">Email Me The
                 App
